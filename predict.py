241086d9390173fbe6b0073d6217c3c7c7ad383d
Pranjali Ghume <pranjalii2003@gmail.com>
2026-04-29 14:11:53 +0530

    Load model from Hugging Face for deployment

--- predict.py
+++ predict.py
@@ -2,7 +2,7 @@ import torch
 from transformers import BertTokenizer, BertForSequenceClassification
 
 # Load saved model
-model_path = "./model"
+MODEL_NAME = "Pranjalii/SentimentModelUpload"
 
 tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 model = BertForSequenceClassification.from_pretrained(model_path)
